7c4918c63aba7c969b927f5085c439bbcd706329
Daria <lampochkat@bk.ru>
2025-10-30 21:57:12 +0300

    feat: icons + colors

---
+++ frontend/src/components/UI/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="headerBg">
+      <p>Welcoome</p>
+      <img src="../../img/leaf.svg" alt="" />
+    </div>
+  )
+}
+
+export default Header
