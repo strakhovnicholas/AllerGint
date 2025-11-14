75dcae6a3c61d53ccb053ed73e31bfaa8a019058
vadzhazha <bura24064@gmail.com>
2025-11-14 12:47:52 +0300

    feat (FilterMap): complete style for component

--- frontend/src/components/pages/Map/Map.tsx
+++ frontend/src/components/pages/Map/Map.tsx
@@ -1,8 +1,16 @@
+import FilterMap from './FilterMap/FilterMap'
 import HeaderMap from './HeaderMap/HeaderMap'
 import style from './Map.module.css'
 
 function Map() {
-   return <HeaderMap />
+   return (
+      <>
+         <HeaderMap />
+         <div className={style.filter}>
+            <FilterMap />
+         </div>
+      </>
+   )
 }
 
 export default Map
