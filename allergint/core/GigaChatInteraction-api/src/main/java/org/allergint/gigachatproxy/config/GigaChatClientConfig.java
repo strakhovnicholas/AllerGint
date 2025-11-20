00253dcc8f059383dcfeecfafc9c3e746e1ce9c1
Nicholas <strahovnikolaj27@gmail.com>
2025-11-20 23:47:15 +0300

    Giga chat microservice config added

---
+++ allergint/core/GigaChatInteraction-api/src/main/java/org/allergint/gigachatproxy/config/GigaChatClientConfig.java
@@ -0,0 +1,29 @@
+package org.allergint.gigachatproxy.config;
+
+import chat.giga.client.GigaChatClient;
+import chat.giga.client.auth.AuthClient;
+import chat.giga.client.auth.AuthClientBuilder;
+import chat.giga.model.Scope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GigaChatClientConfig {
+
+    @Value("${gigachat.auth-key}")
+    private String authKey;
+
+    @Bean
+    public GigaChatClient gigaChatClient() {
+        return GigaChatClient.builder()
+                .verifySslCerts(false)
+                .authClient(AuthClient.builder()
+                        .withOAuth(AuthClientBuilder.OAuthBuilder.builder()
+                                .scope(Scope.GIGACHAT_API_PERS)
+                                .authKey(authKey)
+                                .build())
+                        .build())
+                .build();
+    }
+}
