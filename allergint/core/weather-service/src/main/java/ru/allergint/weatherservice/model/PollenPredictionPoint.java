1c39767dce1d9fdb38c4266c7f1937344b787b04
Nicholas <strahovnikolaj27@gmail.com>
2025-11-25 00:46:12 +0300

    feat: add GigaChat client for AI integration

--- allergint/core/weather-service/src/main/java/ru/allergint/weatherservice/model/PollenPredictionPoint.java
+++
@@ -1,9 +0,0 @@
-package ru.allergint.weatherservice.model;
-
-import java.util.Map;
-
-public class PollenPredictionPoint {
-    private double lat;
-    private double lon;
-    private Map<PollenSpecies, Double> concentration;
-}
