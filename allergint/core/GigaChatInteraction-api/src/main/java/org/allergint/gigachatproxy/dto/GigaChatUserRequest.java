972bf0d59661ea0b280f6fdad2681a0ae84923af
Pavel <sprt4758@gmail.com>
2025-11-26 02:45:19 +0300

    stable docker build, (TODO fix database issues and ai api)
    Signed-off-by: Pavel <sprt4758@gmail.com>

--- allergint/core/GigaChatInteraction-api/src/main/java/org/allergint/gigachatproxy/dto/GigaChatUserRequest.java
+++ allergint/core/GigaChatInteraction-api/src/main/java/org/allergint/gigachatproxy/dto/GigaChatUserRequest.java
@@ -3,12 +3,14 @@ package org.allergint.gigachatproxy.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GigaChatUserRequest {
     private UUID clientId;
     private String message;
