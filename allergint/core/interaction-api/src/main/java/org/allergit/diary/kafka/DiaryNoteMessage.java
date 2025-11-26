cedc61f64d98f9c52bd14a1f491a45e30b8f5509
Pavel <sprt4758@gmail.com>
2025-11-26 23:50:33 +0300

    remove not null on weather
    Signed-off-by: Pavel <sprt4758@gmail.com>

--- allergint/core/interaction-api/src/main/java/org/allergit/diary/kafka/DiaryNoteMessage.java
+++ allergint/core/interaction-api/src/main/java/org/allergit/diary/kafka/DiaryNoteMessage.java
@@ -1,6 +1,7 @@
 package org.allergit.diary.kafka;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.allergit.diary.dto.MedicineDto;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class DiaryNoteMessage {
     private UUID userId;
     private UUID diaryPageId;
