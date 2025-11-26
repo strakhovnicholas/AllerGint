b96dd78a4b6102d027085f15a689fff94229f3c5
Pavel <sprt4758@gmail.com>
2025-11-26 05:03:58 +0300

    latest stable docker-based build TODO fix gigachat interaction
    Signed-off-by: Pavel <sprt4758@gmail.com>

--- allergint/core/diary-service/src/main/resources/schema.sql
+++ allergint/core/diary-service/src/main/resources/schema.sql
@@ -2,37 +2,34 @@ CREATE TABLE IF NOT EXISTS diary
 (
     id
     UUID
-    DEFAULT
-    RANDOM_UUID
-(
-),
-    user_id UUID NOT NULL,
-    health_state VARCHAR
+        PRIMARY
+            KEY,
+    user_id
+    UUID
+        NOT
+            NULL,
+    health_state
+    VARCHAR
 (
     50
 ),
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
                             user_notes VARCHAR (1000),
-    PRIMARY KEY
-(
-    id
+    ai_notes VARCHAR
+(2000
 )
     );
 
-ALTER TABLE diary
-    ADD COLUMN IF NOT EXISTS ai_notes VARCHAR (2000);
-
-CREATE INDEX idx_diary_user_timestamp ON diary (user_id, timestamp);
+CREATE INDEX IF NOT EXISTS idx_diary_user_timestamp ON diary (user_id, timestamp);
 
 CREATE TABLE IF NOT EXISTS weather
 (
     id
     UUID
-    DEFAULT
-    RANDOM_UUID
-(
-),
-    weather_condition VARCHAR
+        PRIMARY
+            KEY,
+    weather_condition
+    VARCHAR
 (
     50
 ),
@@ -43,22 +40,17 @@ CREATE TABLE IF NOT EXISTS weather
 (
     id
 )
-                        ON DELETE CASCADE,
-    PRIMARY KEY
-(
-    id
-)
+                                ON DELETE CASCADE
     );
 
 CREATE TABLE IF NOT EXISTS medicine
 (
     id
     UUID
-    DEFAULT
-    gen_random_uuid
-(
-),
-    medicine_name VARCHAR
+        PRIMARY
+            KEY,
+    medicine_name
+    VARCHAR
 (
     255
 ),
@@ -76,10 +68,6 @@ CREATE TABLE IF NOT EXISTS medicine
 (
     50
 ),
-    PRIMARY KEY
-(
-    id
-),
     CONSTRAINT fk_medicine_diary FOREIGN KEY
 (
     diary_page_id
@@ -93,21 +81,16 @@ CREATE TABLE IF NOT EXISTS user_symptom
 (
     id
     UUID
-    DEFAULT
-    gen_random_uuid
-(
-),
-    symptom_name VARCHAR
+        PRIMARY
+            KEY,
+    symptom_name
+    VARCHAR
 (
     255
 ) NOT NULL,
     diary_page_id UUID NOT NULL,
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL,
                             symptom_state VARCHAR (50),
-    PRIMARY KEY
-(
-    id
-),
     CONSTRAINT fk_symptom_diary FOREIGN KEY
 (
     diary_page_id
@@ -122,24 +105,19 @@ CREATE TABLE IF NOT EXISTS allergy_trigger
 (
     id
     UUID
-    DEFAULT
-    gen_random_uuid
-(
-),
-    trigger_name VARCHAR
+        PRIMARY
+            KEY,
+    trigger_name
+    VARCHAR
 (
     255
 ) NOT NULL,
-    PRIMARY KEY
-(
-    id
-),
     trigger_state VARCHAR
 (
     50
 )
     );
 
-CREATE INDEX idx_medicine_diary ON medicine (diary_page_id);
-CREATE INDEX idx_symptom_diary ON user_symptom (diary_page_id);
-CREATE INDEX idx_weather_diary ON weather (diary_page_id);
+CREATE INDEX IF NOT EXISTS idx_medicine_diary ON medicine (diary_page_id);
+CREATE INDEX IF NOT EXISTS idx_symptom_diary ON user_symptom (diary_page_id);
+CREATE INDEX IF NOT EXISTS idx_weather_diary ON weather (diary_page_id);
