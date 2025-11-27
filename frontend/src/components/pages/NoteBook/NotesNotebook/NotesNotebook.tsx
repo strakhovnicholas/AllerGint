3b43ec8dc9180030ad214267ffbe52b87d53c0fd
Daria <lampochkat@bk.ru>
2025-11-27 20:39:10 +0300

    feat (Notebook page): complete notes card

--- frontend/src/components/pages/NoteBook/NotesNotebook/NotesNotebook.tsx
+++ frontend/src/components/pages/NoteBook/NotesNotebook/NotesNotebook.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import styles from './NotesNotebook.module.css'
 
 function NotesNotebook() {
   return (
-    <div className="card">
-      <p className="title">Заметки</p>
+    <div className={`card ${styles.cardNotes}`}>
+      <div className={styles.notesTitle}>
+        <p className="title">Заметки</p>
+      </div>
+      <div className={styles.notesList}>
+        <div className={styles.noteListItem}>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus quidem non magnam pariatur architecto perferendis
+            cupiditate maxime, nostrum debitis dolore?
+          </p>
+          <div className={styles.noteListSub}>
+            <p className={styles.noteSubtitle}>
+              Добавлено в <span>saved time</span>
+            </p>
+          </div>
+        </div>
+        <div className={styles.noteWrite}>
+          <textarea
+            className={styles.noteInput}
+            name="keyboard"
+            placeholder="Добавьте заметку о самочувствии, активности или других наблюдениях..."
+          ></textarea>
+          <button className={styles.noteAddBtn}>Добавить запись</button>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/pages/Profile/MedProfile/MedProfile.tsx
+++ frontend/src/components/pages/Profile/MedProfile/MedProfile.tsx
@@ -1,54 +1,52 @@
 import style from './MedProfile.module.css'
 
 function MedProfile() {
-   const al = [
-      { type: 'Полынь', level: 1 },
-      { type: 'Берёза', level: 2 },
-      { type: 'Дуб', level: 3 },
-      { type: 'Кошки', level: 1 },
-   ]
+  const al = [
+    { type: 'Полынь', level: 1 },
+    { type: 'Берёза', level: 2 },
+    { type: 'Дуб', level: 3 },
+    { type: 'Кошки', level: 1 },
+  ]
 
-   const months = ['январь', 'февраль', 'март']
+  const months = ['январь', 'февраль', 'март']
 
-   const chastota = 'Редко'
+  const chastota = 'Редко'
 
-   return (
-      <div className="card">
-         <div className="title">Мои аллергены</div>
+  return (
+    <div className="card">
+      <div className="title">Мои аллергены</div>
 
-         <div className={style.severity}>
-            <div>
-               Сезонность:{' '}
-               {months.map((val, index) => {
-                  return <span key={index}>{val}</span>
-               })}
-            </div>
-            <div>
-               Частота симптомов: <span>{chastota}</span>
-            </div>
-         </div>
-
-         <ul className={style.data}>
-            {al.map((val, index) => {
-               return (
-                  <li className={style.listItem} key={index}>
-                     <div className={style.text}>{val.type}</div>
-                     {val.level === 1 && (
-                        <div className={style.weak}>слабо</div>
-                     )}
-                     {val.level === 2 && (
-                        <div className={style.medium}>средне</div>
-                     )}
-                     {val.level === 3 && (
-                        <div className={style.strong}>сильно</div>
-                     )}
-                  </li>
-               )
-            })}
-         </ul>
-         <button className={style.edit}>Добавить</button>
+      <div className={style.severity}>
+        <div>
+          Сезонность:{' '}
+          {months.map((val, index) => {
+            return (
+              <span className={style.span} key={index}>
+                {val}
+              </span>
+            )
+          })}
+        </div>
+        <div>
+          Частота симптомов: <span className={style.span}>{chastota}</span>
+        </div>
       </div>
-   )
+
+      <ul className={style.data}>
+        {al.map((val, index) => {
+          return (
+            <li className={style.listItem} key={index}>
+              <div className={style.text}>{val.type}</div>
+              {val.level === 1 && <div className={style.weak}>слабо</div>}
+              {val.level === 2 && <div className={style.medium}>средне</div>}
+              {val.level === 3 && <div className={style.strong}>сильно</div>}
+            </li>
+          )
+        })}
+      </ul>
+      <button className={style.edit}>Добавить</button>
+    </div>
+  )
 }
 
 export default MedProfile
